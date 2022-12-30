eb522952b046b81fc7f2a743b2aac5109202ebcc
kusham <kusham1998@gmail.com>
2022-12-30 23:07:25 +0530

    add few components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css'
+import CompanyFeatures from './components/CompanyFeatures/CompanyFeatures';
+import ImageSlideShow from './components/ImageSlideShow/ImageSlideShow';
+import IntroVideo from './components/IntroVideo/IntroVideo';
 import Navbar from './components/Navbar/Navbar';
+import RegisterSection from './components/RegisterSection/RegisterSection';
 
 function App() {
   return (
     <div >
       <Navbar />
+      <ImageSlideShow />
+      <IntroVideo />
+      <RegisterSection />
+      <CompanyFeatures />
     </div>
   );
 }
