804c075e4b159289bd4d7da08528681d8a448699
kusham <kusham1998@gmail.com>
2022-12-31 20:53:14 +0530

    create slide data file

---
+++ src/Resources/Data/Slides.jsx
@@ -0,0 +1,49 @@
+import image_1 from "../../Resources/Images/1.png";
+import image_2 from "../../Resources/Images/2.png";
+import image_3 from "../../Resources/Images/3.png";
+import image_4 from "../../Resources/Images/4.jpg";
+import image_5 from "../../Resources/Images/5.jpg";
+
+
+export const slideItem = [
+    {
+      active: true,
+      image: image_2,
+      alt: "banner one",
+      title: "First slide Title",
+      description:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid quam porro architecto illum voluptatem dolorem quidem ratione labore, vero a impedit atque velit numquam natus nam eum quaerat possimus dicta.",
+    },
+    {
+        active: false,
+        image: image_3,
+        alt: "banner two",
+        title: "second slide Title",
+        description:
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid quam porro architecto illum voluptatem dolorem quidem ratione labore, vero a impedit atque velit numquam natus nam eum quaerat possimus dicta.",
+      },
+      {
+        active: false,
+        image: image_4,
+        alt: "banner three",
+        title: "Third slide Title",
+        description:
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid quam porro architecto illum voluptatem dolorem quidem ratione labore, vero a impedit atque velit numquam natus nam eum quaerat possimus dicta.",
+      },
+      {
+        active: false,
+        image: image_5,
+        alt: "banner four",
+        title: "Fourth slide Title",
+        description:
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid quam porro architecto illum voluptatem dolorem quidem ratione labore, vero a impedit atque velit numquam natus nam eum quaerat possimus dicta.",
+      },
+      {
+        active: false,
+        image: image_1,
+        alt: "banner five",
+        title: "Fifth slide Title",
+        description:
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid quam porro architecto illum voluptatem dolorem quidem ratione labore, vero a impedit atque velit numquam natus nam eum quaerat possimus dicta.",
+      },
+  ];
