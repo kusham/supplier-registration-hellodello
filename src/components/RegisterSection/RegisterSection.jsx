f221beb0971a9be4a7e12094f6ff1e25cc642e0d
kusham <kusham1998@gmail.com>
2022-12-31 20:54:10 +0530

    add map

--- src/components/RegisterSection/RegisterSection.jsx
+++ src/components/RegisterSection/RegisterSection.jsx
@@ -2,19 +2,34 @@ import React from "react";
 import "./RegisterSectionStyle.css";
 import twoGirls from "../../Resources/Images/two-girls.png";
 
+
 const RegisterSection = () => {
+
   return (
     <div>
       <div className="container mt-5">
         <div className="row">
-          <div className="col-sm-12 col-md-6 RegisterSection-image">
+          <div
+            data-aos="fade-right"
+            data-aos-duration="1000"
+            className="col-sm-12 col-md-6 RegisterSection-image"
+          >
             <img src={twoGirls} alt="twoGirls" />
           </div>
+
           <div className="col-sm-12 col-md-6 mt-sm-5 RegisterSection-text">
-            <h1>Merchant </h1>
-            <h1>Registration</h1>
-            <p>Let’s build something special !</p>
-            <button>ENROLL</button>
+            <h1 data-aos="fade-left" data-aos-duration="1000">
+              Merchant{" "}
+            </h1>
+            <h1 data-aos="fade-left" data-aos-duration="1000">
+              Registration
+            </h1>
+            <p data-aos="fade-left" data-aos-duration="2000">
+              Let’s build something special !
+            </p>
+            <button data-aos="fade-left" data-aos-duration="3000">
+              ENROLL
+            </button>
           </div>
         </div>
       </div>
