5c6b5f2fca336638ab5c4d55633b4cf06c9fd024
kusham <kusham1998@gmail.com>
2023-01-09 23:46:21 +0530

    style change

--- src/components/RegisterModal/ModalTitle.jsx
+++ src/components/RegisterModal/ModalTitle.jsx
@@ -5,7 +5,7 @@ const ModalTitle = () => {
   return (
     <div className="ModalTitle-container">
       <img className="Navbar-logo" src={logo} alt="HelloDello-logo" />
-      <h1>Sell With HelloDello</h1>
+      <h2>Sell With HelloDello</h2>
     </div>
   );
 };
