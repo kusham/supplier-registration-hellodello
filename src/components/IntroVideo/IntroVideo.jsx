cff937cb683025fe91c22f62c8f395491d4d1428
kusham <kusham1998@gmail.com>
2023-01-09 23:44:02 +0530

    format file

--- src/components/IntroVideo/IntroVideo.jsx
+++ src/components/IntroVideo/IntroVideo.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import introduction from "../../Resources/Videos/introduction.mp4";
-import './IntroVideoStyle.css'
+import "./IntroVideoStyle.css";
 
 const IntroVideo = () => {
   return (
     <div className="IntroVideo-content">
       <div className="IntroVideo-title">
-        <h1 data-aos="fade-left"
-            data-aos-duration="1000">WHO ARE WE ?</h1>
+        <h1 data-aos="fade-left" data-aos-duration="1000">
+          WHO ARE WE ?
+        </h1>
       </div>
-      <div className="IntroVideo-video" data-aos="fade-up"
-            data-aos-duration="1000">
+      <div
+        className="IntroVideo-video"
+        data-aos="fade-up"
+        data-aos-duration="1000"
+      >
         <video width="100%" loop muted autoPlay>
           <source src={introduction} type="video/mp4" />
           Error Message
