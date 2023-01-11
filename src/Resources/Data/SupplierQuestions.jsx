b974d326616a68f0008949922dfb39b0e967dd08
kusham <kusham1998@gmail.com>
2023-01-11 22:55:15 +0530

    create separate question file

---
+++ src/Resources/Data/SupplierQuestions.jsx
@@ -0,0 +1,177 @@
+import image1 from "../../Resources/Images/6.png";
+import image2 from "../../Resources/Images/7.png";
+import image3 from "../../Resources/Images/8.png";
+import image4 from "../../Resources/Images/9.png";
+
+export const questions = [
+    {
+      id: 1,
+      title: "Welcome to HELLODELLO",
+      label: "Email:",
+      placeholder: "Your email",
+      InputName: "email",
+      image: image1,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae eveniet eius vero accusamus expedita eaque labore non ad excepturi! Eius",
+      prevId: null,
+      nextId: 2,
+    },
+  
+    {
+      id: 2,
+      title: "Name of Your brand",
+      label: "Brand name:",
+      placeholder: "Enter Your brand name",
+      InputName: "brandName",
+      image: image2,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae eveniet eius vero accusamus expedita eaque labore non ad excepturi! Eius",
+      prevId: 1,
+      nextId: 3,
+    },
+  
+    {
+      id: 3,
+      title: "company Establishment",
+      label: "Year of Establishment:",
+      placeholder: "Select year",
+      InputName: "establishmentYear",
+      image: image3,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae eveniet eius vero accusamus expedita eaque labore non ad excepturi! Eius",
+      prevId: 2,
+      nextId: 4,
+    },
+    {
+      id: 4,
+      title: "Welcome to HELLODELLO",
+      label: "Email:",
+      placeholder: "Your email",
+      InputName: "email",
+      image: image4,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae eveniet eius vero accusamus expedita eaque labore non ad excepturi! Eius",
+      prevId: 3,
+      nextId: 5,
+    },
+    {
+      id: 5,
+      title: "Welcome to HELLODELLO",
+      label: "Email:",
+      placeholder: "Your email",
+      InputName: "email",
+      image: image1,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae eveniet eius vero accusamus expedita eaque labore non ad excepturi! Eius",
+      prevId: 4,
+      nextId: 6,
+    },
+    {
+      id: 6,
+      title: "Welcome to HELLODELLO",
+      label: "Email:",
+      placeholder: "Your email",
+      InputName: "email",
+      image: image2,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae eveniet eius vero accusamus expedita eaque labore non ad excepturi! Eius",
+      prevId: 5,
+      nextId: 7,
+    },
+    {
+      id: 7,
+      title: "Welcome to HELLODELLO",
+      label: "Email:",
+      placeholder: "Your email",
+      InputName: "email",
+      image: image3,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae eveniet eius vero accusamus expedita eaque labore non ad excepturi! Eius",
+      prevId: 6,
+      nextId: 8,
+    },
+    {
+      id: 8,
+      title: "Welcome to HELLODELLO",
+      label: "Email:",
+      placeholder: "Your email",
+      InputName: "email",
+      image: image4,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae eveniet eius vero accusamus expedita eaque labore non ad excepturi! Eius",
+      prevId: 7,
+      nextId: 9,
+    },
+    {
+      id: 9,
+      title: "Welcome to HELLODELLO",
+      label: "Email:",
+      placeholder: "Your email",
+      InputName: "email",
+      image: image1,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae eveniet eius vero accusamus expedita eaque labore non ad excepturi! Eius",
+      prevId: 8,
+      nextId: 10,
+    },
+    {
+      id: 10,
+      title: "Welcome to HELLODELLO",
+      label: "Email:",
+      placeholder: "Your email",
+      InputName: "email",
+      image: image2,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae eveniet eius vero accusamus expedita eaque labore non ad excepturi! Eius",
+      prevId: 9,
+      nextId: 11,
+    },
+    {
+      id: 11,
+      title: "Welcome to HELLODELLO",
+      label: "Email:",
+      placeholder: "Your email",
+      InputName: "email",
+      image: image3,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae eveniet eius vero accusamus expedita eaque labore non ad excepturi! Eius",
+      prevId: 10,
+      nextId: 12,
+    },
+    {
+      id: 12,
+      title: "Welcome to HELLODELLO",
+      label: "Email:",
+      placeholder: "Your email",
+      InputName: "email",
+      image: image4,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae eveniet eius vero accusamus expedita eaque labore non ad excepturi! Eius",
+      prevId: 11,
+      nextId: 13,
+    },
+    {
+      id: 13,
+      title: "Welcome to HELLODELLO",
+      label: "Email:",
+      placeholder: "Your email",
+      InputName: "email",
+      image: image1,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae eveniet eius vero accusamus expedita eaque labore non ad excepturi! Eius",
+      prevId: 12,
+      nextId: 14,
+    },
+    {
+      id: 14,
+      title: "Welcome to HELLODELLO",
+      label: "Email:",
+      placeholder: "Your email",
+      InputName: "email",
+      image: image2,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae eveniet eius vero accusamus expedita eaque labore non ad excepturi! Eius",
+      prevId: 13,
+      nextId: null,
+    },
+  ];
