75ff06a3e66496e38909b692dfe079fa6d70a33e
kusham <kusham1998@gmail.com>
2023-01-13 22:22:31 +0530

    move the modal

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import { useState } from 'react';
+import React from 'react';
 import './App.css'
 import CompanyFeatures from './components/CompanyFeatures/CompanyFeatures';
 import Footer from './components/Footer/Footer';
 import ImageSlideShow from './components/ImageSlideShow/ImageSlideShow';
 import IntroVideo from './components/IntroVideo/IntroVideo';
 import Navbar from './components/Navbar/Navbar';
-import RegisterModal from './components/RegisterModal/RegisterModal';
 import RegisterSection from './components/RegisterSection/RegisterSection';
 
 function App() {
-  const [openRegistration, setOpenRegistration] = useState(false)
   return (
     <div >
       <Navbar />
       <ImageSlideShow />
       <IntroVideo />
-      <RegisterSection setOpenRegistration={setOpenRegistration}/>
-      <RegisterModal openRegistration={openRegistration} setOpenRegistration={setOpenRegistration}/>
+      <RegisterSection />
       <CompanyFeatures />
       <Footer />
     </div>
