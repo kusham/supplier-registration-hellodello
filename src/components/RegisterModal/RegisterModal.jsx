270439009dfceb88e5494b53cc2438832ae47289
kusham <kusham1998@gmail.com>
2023-01-13 22:35:50 +0530

    add background blur color

--- src/components/RegisterModal/RegisterModal.jsx
+++ src/components/RegisterModal/RegisterModal.jsx
@@ -1,29 +1,27 @@
-import React from 'react';
-import { Modal } from '@mantine/core';
-import './RegisterModalStyle.css'
-import RegisterForm from '../RegisterForm/RegisterForm';
-import ModalTitle from './ModalTitle';
+import React from "react";
+import { Modal } from "@mantine/core";
+import "./RegisterModalStyle.css";
+import RegisterForm from "../RegisterForm/RegisterForm";
+import ModalTitle from "./ModalTitle";
 
-const RegisterModal = ({openRegistration, handleCloseModal}) => {
-    
+const RegisterModal = ({ openRegistration, handleCloseModal }) => {
   return (
-    
     <Modal
-    opened={openRegistration}
-    onClose={handleCloseModal}
-    transition="scale"
-    transitionDuration={1600}
-    transitionTimingFunction="ease"
-    fullScreen
-    title={<ModalTitle />}
-  >
-<div className='RegisterModal-container'>
-  <RegisterForm handleCloseModal={handleCloseModal}/>
-</div>
-  </Modal>
-        
-  )
-}
+      opened={openRegistration}
+      onClose={handleCloseModal}
+      transition="scale"
+      transitionDuration={1600}
+      transitionTimingFunction="ease"
+      fullScreen
+      title={<ModalTitle />}
+    >
+      <div className="RegisterModal-container">
+        <div className="blur" style={{ top: "2%", right: "0" }}></div>
+        <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
+        <RegisterForm handleCloseModal={handleCloseModal} />
+      </div>
+    </Modal>
+  );
+};
 
-export default RegisterModal
-
+export default RegisterModal;
