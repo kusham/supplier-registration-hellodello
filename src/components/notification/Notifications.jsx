f04684655fb6faa05426f8a707ddb1279e5a3d29
kusham <kusham1998@gmail.com>
2023-01-16 00:14:25 +0530

    change the notification

--- src/components/notification/Notifications.jsx
+++ src/components/notification/Notifications.jsx
@@ -1,16 +1,33 @@
+import { Button } from "@mantine/core";
 import React from "react";
-import { Notification } from '@mantine/core';
+import "./NotificationStyle.css";
 
-const Notifications = () => {
+const Notifications = ({ isSucceed, handleCloseModal }) => {
   return (
-    <Notification
-      icon={<i class="fa fa-check"></i>}
-      color="green"
-      radius="lg"
-      title="Registration Successful"
-    >
-      All your details have been saved successfully.
-    </Notification>
+    <div className="Notifications-container">
+      {isSucceed ? (
+        <>
+          <i
+            style={{ backgroundColor: "rgb(11, 185, 11)" }}
+            className="fa fa-check"
+          ></i>
+          <h4>Registration Successful</h4>
+          <p>All your details have been saved successfully.</p>
+        </>
+      ) : (
+        <>
+          <i
+            style={{ backgroundColor: "red" }}
+            className="fa fa-circle-xmark"
+          ></i>
+          <h4 style={{ color: "red" }}>Something went wrong !</h4>
+          <p>Thank you for trying to register. Please try again later.</p>
+        </>
+      )}
+      <Button color="dark" radius="xl" uppercase onClick={handleCloseModal}>
+        close
+      </Button>
+    </div>
   );
 };
 
