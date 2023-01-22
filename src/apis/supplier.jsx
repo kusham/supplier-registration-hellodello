7060db540d1038d71fcf3d963386474dc3b34c07
kusham <kusham1998@gmail.com>
2023-01-22 05:57:08 +0530

    changes for api actions

--- src/apis/supplier.jsx
+++ src/apis/supplier.jsx
@@ -1,36 +1,33 @@
-import axios from 'axios'
+import axios from "axios";
 
-const API = axios.create({ baseURL: 'http://localhost:8070' });
+const API = axios.create({ baseURL: "http://localhost:8070" });
 
-export const checkEmail = async(email)=> {
-    try {
-        console.log(email);
-        const {data} = await API.post(`/supplier/check/email/${email}`);
-        return data.success
-    } catch (error) {
-        console.log(error.response.data)
-    }
+export const checkEmail = async (email) => {
+  try {
+    console.log(email);
+    const { data } = await API.post(`/supplier/check/email/${email}`);
+    return data.success;
+  } catch (error) {
+    return error.response.data.success;
+  }
+};
 
-}
+export const checkBrandName = async (brandName) => {
+  try {
+    const { data } = await API.post(`/supplier/check/brandName/${brandName}`);
+    return data.success;
+  } catch (error) {
+    return error.response.data.success;
 
-export const checkBrandName = async(brandName)=> {
-    try {
-        const {data} = await API.get(`/supplier/check/brandName${brandName}`);
-        console.log(data);
-    } catch (error) {
-        console.log(error.message)
-    }
+  }
+};
 
-}
-
-
-export const saveSupplier = async(supplierData)=> {
-    try {
-        const res = await API.post(`/supplier/add`, supplierData);
-        console.log(res);
-         return res.data;
-    } catch (error) {
-        console.log(error.message)
-    }
-
-}
+export const saveSupplier = async (supplierData) => {
+  try {
+    const res = await API.post(`/supplier/add`, supplierData);
+    console.log(res);
+    return res.data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
