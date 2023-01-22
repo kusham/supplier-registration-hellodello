8d36e1527986299929fe09a1ea7f350164bdc73d
kusham <kusham1998@gmail.com>
2023-01-22 05:59:32 +0530

    add hosted backend url

--- src/apis/supplier.jsx
+++ src/apis/supplier.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:8070" });
+const API = axios.create({ baseURL: "https://hello-dello.onrender.com" });
 
 export const checkEmail = async (email) => {
   try {
