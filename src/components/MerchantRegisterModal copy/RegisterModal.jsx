c575456a0b0df34ad51583f4ab750dee7f07f1d7
kusham <kusham1998@gmail.com>
2023-04-09 13:48:10 +0530

    create RegisterModal com

---
+++ src/components/MerchantRegisterModal copy/RegisterModal.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { Modal } from "@mantine/core";
+import "./RegisterModalStyle.css";
+import RegisterForm from "../RegisterForm/RegisterFormMerchant";
+import ModalTitle from "./ModalTitle";
+
+const RegisterModal = ({ openRegistration, handleCloseModal }) => {
+  const [isSucceed, setIsSucceed] = useState(false);
+  const [isError, setIsError] = useState(false);
+  return (
+    <Modal
+      opened={openRegistration}
+      onClose={handleCloseModal}
+      transition="scale"
+      transitionDuration={1600}
+      transitionTimingFunction="ease"
+      fullScreen
+      title={!isError && !isSucceed && <ModalTitle />}
+    >
+      <div className="RegisterModal-container">
+        <div className="blur1"></div>
+        <div className="blur2"></div>
+        <RegisterForm
+          handleCloseModal={handleCloseModal}
+          isSucceed={isSucceed}
+          setIsSucceed={setIsSucceed}
+          isError={isError}
+          setIsError={setIsError}
+        />
+      </div>
+    </Modal>
+  );
+};
+
+export default RegisterModal;
